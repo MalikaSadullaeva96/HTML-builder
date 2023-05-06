71159077fce0564b510b095e9b24105d8ea74349
MalikaSadullaeva96 <malika@Malikas-MacBook-Pro.local>
2023-05-06 20:06:13 +0500

    feat: write to file

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const filePath = path.join(__dirname, 'text.txt');
+
+stdout.write('Hi! Please enter text\n');
+
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+stdin.on('data', data => {
+  const input = data.toString().trim();
+  if (input === 'exit') {
+    stdout.write('Goodbye!\n');
+    process.exit();
+  } else {
+    writeStream.write(input + '\n');
+    stdout.write('Hi! Please enter text:\n');
+  }
+});
+process.on('SIGINT', function() {
+    console.log('\nGoodbye!\n');
+    process.exit();
+});
