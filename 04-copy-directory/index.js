67c10580d94d4f6aa58e9964a8b6e641fd4118f3
MalikaSadullaeva96 <malika@Malikas-MacBook-Pro.local>
2023-05-09 22:18:49 +0500

    fix: update dir feat

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,19 @@
 const fs = require('fs/promises');
 const path = require('path');
 
+async function emptyDirectory(directory) {
+  const files = await fs.readdir(directory);
+  for (const file of files) {
+    const filePath = path.join(directory, file);
+    const stat = await fs.stat(filePath);
+    if (stat.isFile()) {
+      await fs.unlink(filePath);
+    } else if (stat.isDirectory()) {
+      await emptyDirectory(filePath);
+      await fs.rmdir(filePath);
+    }
+  }
+}
 
 async function copyRecursive(source, destination) {
   const stat = await fs.stat(source);
@@ -17,36 +30,32 @@ async function copyRecursive(source, destination) {
   }
 }
 
-
-function copyFile (sourceDir = 'files', folderName = 'files-copy' ){
-
-
+async function copyFile(sourceDir = 'files', folderName = 'files-copy') {
   const dirPath = path.join(__dirname, sourceDir); // source folder
-  const dirName = path.join(__dirname, folderName);
+  const dirName = path.join(__dirname, folderName); // destination folder
 
+  try {
+    await fs.mkdir(dirName, { recursive: true });
+    console.log(`${dirName} directory created successfully!`);
 
+    await emptyDirectory(dirName);
+    console.log(`${dirName} directory emptied successfully!`);
 
-  fs.mkdir(dirName, { recursive: true })
-    .then(() => {
-      console.log(`${dirName} directory created successfully!`);
-    })
-    .catch((err) => {
-      console.error(`Error creating ${dirName} directory:`, err);
-    });
-
-  
-  fs.readdir(dirPath)
-    .then(async (files) => {
-      for (const file of files) {
-        const sourcePath = path.join(dirPath, file);
-        const destinationPath = path.join(dirName, file);
-        await copyRecursive(sourcePath, destinationPath);
-      }
-    })
-    .catch((err) => {
-      console.error(`Error reading files in ${dirPath} directory:`, err);
-    });
-
+    const files = await fs.readdir(dirPath);
+    for (const file of files) {
+      const sourcePath = path.join(dirPath, file);
+      const destinationPath = path.join(dirName, file);
+      await copyRecursive(sourcePath, destinationPath);
+    }
+    console.log(`Files copied successfully from ${dirPath} to ${dirName}!`);
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      console.error(`Error creating ${dirName} directory: Directory already exists.`);
+    } else {
+      console.error(`Error:`, err);
+    }
+  }
 }
+
 copyFile();
 module.exports = copyFile;
